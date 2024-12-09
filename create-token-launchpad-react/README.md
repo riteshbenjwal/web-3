597327d4570f3411a84a8afe742230940e1bda82
riteshbenjwal <riteshbenjwal7@gmail.com>
2024-12-09 13:46:01 +0530

    add ui and create token with metadata on token program 22

--- create-token-launchpad-react/README.md
+++
@@ -1,2 +0,0 @@
-# Starter code
-The code contains the initial HTML and CSS structure for the token launchpad.
--- create-token-launchpad-react/src/components/TokenLaunchpad.jsx
+++ create-token-launchpad-react/src/components/TokenLaunchpad.jsx
@@ -1,17 +1,203 @@
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  ExtensionType,
+  LENGTH_SIZE,
+  TOKEN_2022_PROGRAM_ID,
+  TYPE_SIZE,
+  createAssociatedTokenAccountInstruction,
+  createInitializeMetadataPointerInstruction,
+  createInitializeMintInstruction,
+  createMintToInstruction,
+  getAssociatedTokenAddressSync,
+  getMintLen,
+} from "@solana/spl-token";
+import { createInitializeInstruction, pack } from "@solana/spl-token-metadata";
+import { useState } from "react";
 
 export function TokenLaunchpad() {
-    return  <div style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column'
-    }}>
-        <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' placeholder='Name'></input> <br />
-        <input className='inputText' type='text' placeholder='Symbol'></input> <br />
-        <input className='inputText' type='text' placeholder='Image URL'></input> <br />
-        <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+  const { connection } = useConnection();
+  const wallet = useWallet();
+
+  const [name, setName] = useState("");
+  const [symbol, setSymbol] = useState("");
+  const [uri, setUri] = useState("");
+  const [initialSupply, setInitialSupply] = useState("");
+
+  const DECIMALS = 9;
+
+  async function createMintTransaction(mintKeypair, lamports, mintLen) {
+    const transaction = new Transaction().add(
+      SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey,
+        newAccountPubkey: mintKeypair.publicKey,
+        space: mintLen,
+        lamports,
+        programId: TOKEN_2022_PROGRAM_ID,
+      })
+    );
+    return transaction;
+  }
+
+  async function attachMetaData(mintKeypair, metadata) {
+    const transaction = new Transaction().add(
+      createInitializeMetadataPointerInstruction(
+        mintKeypair.publicKey,
+        wallet.publicKey,
+        mintKeypair.publicKey,
+        TOKEN_2022_PROGRAM_ID
+      ),
+      createInitializeMintInstruction(
+        mintKeypair.publicKey,
+        DECIMALS,
+        wallet.publicKey,
+        null,
+        TOKEN_2022_PROGRAM_ID
+      ),
+      createInitializeInstruction({
+        programId: TOKEN_2022_PROGRAM_ID,
+        mint: mintKeypair.publicKey,
+        metadata: mintKeypair.publicKey,
+        name: metadata.name,
+        symbol: metadata.symbol,
+        uri: metadata.uri,
+        mintAuthority: wallet.publicKey,
+        updateAuthority: wallet.publicKey,
+      })
+    );
+    return transaction;
+  }
+
+  async function createToken() {
+    try {
+      const mintKeypair = Keypair.generate();
+
+      const metadata = {
+        mint: mintKeypair.publicKey,
+        name: name,
+        symbol: symbol,
+        uri: uri,
+        additionalMetadata: [],
+      };
+      const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+      const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length;
+
+      const lamports = await connection.getMinimumBalanceForRentExemption(
+        mintLen + metadataLen
+      );
+
+      const transaction1 = await createMintTransaction(
+        mintKeypair,
+        lamports,
+        mintLen
+      );
+
+      const transaction2 = await attachMetaData(mintKeypair, metadata);
+
+      const combinedTransaction = new Transaction();
+      combinedTransaction.add(...transaction1.instructions);
+      combinedTransaction.add(...transaction2.instructions);
+
+      combinedTransaction.feePayer = wallet.publicKey;
+      combinedTransaction.recentBlockhash = (
+        await connection.getLatestBlockhash()
+      ).blockhash;
+
+      combinedTransaction.partialSign(mintKeypair);
+
+      await wallet.sendTransaction(combinedTransaction, connection);
+
+      console.log(`Token mint created at ${mintKeypair.publicKey.toBase58()}`);
+
+      const associatedToken = getAssociatedTokenAddressSync(
+        mintKeypair.publicKey,
+        wallet.publicKey,
+        false,
+        TOKEN_2022_PROGRAM_ID
+      );
+
+      const transactionAta = new Transaction().add(
+        createAssociatedTokenAccountInstruction(
+          wallet.publicKey,
+          associatedToken,
+          wallet.publicKey,
+          mintKeypair.publicKey,
+          TOKEN_2022_PROGRAM_ID
+        )
+      );
+
+      await wallet.sendTransaction(transactionAta, connection);
+
+      console.log("Minting tokens to ATA: ", associatedToken.toBase58());
+
+      const scaledInitialSupply =
+        BigInt(initialSupply) * BigInt(10 ** DECIMALS);
+
+      const transaction3 = new Transaction().add(
+        createMintToInstruction(
+          mintKeypair.publicKey,
+          associatedToken,
+          wallet.publicKey,
+          scaledInitialSupply,
+          [],
+          TOKEN_2022_PROGRAM_ID
+        )
+      );
+
+      await wallet.sendTransaction(transaction3, connection);
+
+      console.log("Minted!");
+    } catch (error) {
+      console.error("Error creating token:", error);
+    }
+  }
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Solana Token Launchpad</h1>
+      <input
+        className="inputText"
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <input
+        className="inputText"
+        type="text"
+        placeholder="Symbol"
+        value={symbol}
+        onChange={(e) => setSymbol(e.target.value)}
+      />
+      <br />
+      <input
+        className="inputText"
+        type="text"
+        placeholder="Image URL"
+        value={uri}
+        onChange={(e) => setUri(e.target.value)}
+      />
+      <br />
+      <input
+        className="inputText"
+        type="text"
+        placeholder="Initial Supply"
+        value={initialSupply}
+        onChange={(e) => setInitialSupply(e.target.value)}
+      />
+      <br />
+      <button onClick={createToken} className="btn">
+        Create a token
+      </button>
     </div>
+  );
 }
