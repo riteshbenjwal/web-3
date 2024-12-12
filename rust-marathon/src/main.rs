6e056d1a8e7d163c17aa4c1e69812612ae6df879
riteshbenjwal <riteshbenjwal7@gmail.com>
2024-12-12 12:57:39 +0530

    imp struct

--- rust-marathon/src/main.rs
+++ rust-marathon/src/main.rs
@@ -1,17 +1,28 @@
+/*
+
 fn main() {
-    let ans = fib(10);
-    println!("{}", ans);
+    let name = String::from("harkirat");
+    let len = get_str_len(name);
+    println!("the length of the string is {}", len);
 }
 
-// Function Declartion, check if number is even
-// fn is_even(num: i32) -> bool {
-//     if num % 2 == 0 {
-//         return true;
-//     }
-//     return false;
-// }
+fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        return true;
+    }
+    return false;
+}
+
+*/
 
 // Function Declartion, Fibonaci
+
+/*
+
+fn main() {
+    println!("{}", fib(4));
+}
+
 
 fn fib(n: u32) -> u32 {
     let (mut first, mut second) = (0, 1);
@@ -20,11 +31,68 @@ fn fib(n: u32) -> u32 {
         return first;
     }
 
-    for _ in 2..=n {
+    for _ in 0..(n - 1) {
         let temp = second;
         second += first;
         first = temp;
     }
 
     second
+}
+
+*/
+
+// Struct 
+
+/* 
+struct User {
+    first_name: String,
+    last_name: String,
+    age: i32,
+}
+
+fn main() {
+    let user = User {
+        first_name: String::from("Ritesh"),
+        last_name: String::from("Benjwal"),
+        age: 32,
+    };
+
+    println!("{}", user.first_name);
+}
+
+*/
+
+
+// Implementing Structs
+
+struct Rect {
+    width: i32,
+    height: i32,
+}
+
+impl Rect {
+    fn area(&self) -> i32 {
+        self.width * self.height
+    }
+
+    fn perimeter(&self, num: i32) -> i32 {
+        2 * (self.width + self.height)
+    }
+
+    fn debug() -> i32 {
+        return 1;
+    }
+}
+
+fn main() {
+    let rect1 = Rect {
+        width: 10,
+        height: 20,
+    };
+
+    println!("area is {}", rect1.area());
+    println!("perimeter is {}", rect1.perimeter(1));
+    // println!("debug is {}", Rect::debug());
+    println!("debug is {}", Rect::debug());
 }
