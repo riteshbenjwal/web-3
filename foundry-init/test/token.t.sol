9bb936faa7d51dbc62325f568d3fc22e18038a57
riteshbenjwal <riteshbenjwal7@gmail.com>
2025-02-02 14:33:59 +0530

    add events

--- foundry-init/test/token.t.sol
+++ foundry-init/test/token.t.sol
@@ -6,6 +6,8 @@ import { MyToken } from "../src/token.sol";
 
 contract CounterTest is Test {
     MyToken public token;
+    
+    event Transfer(address indexed from, address indexed to, uint256 amount);
 
     function setUp() public {
         token = new MyToken();
@@ -54,4 +56,26 @@ function testFail_Allowance() public {
     assertEq(token.balanceOf(address(this)), 0);
     assertEq(token.balanceOf(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD), 100);
 }
+
+function test_ExpectEmit() public {
+        token.mint(address(this), 100);
+        // Check that topic 1, topic 2, and data are the same as the following emitted event.
+        // Checking topic 3 here doesn't matter, because `Transfer` only has 2 indexed topics.
+        vm.expectEmit(true, true, false, true);
+        // The event we expect
+        emit Transfer(address(this), 0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD, 100);
+        // The event we get
+        token.transfer(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD, 100);
+    }
+    
+    // function test_ExpectEmitApprove() public {
+    //     token.mint(address(this), 100);
+        
+    //     vm.expectEmit(true, true, false, true);
+    //     emit Approval(address(this), 0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD, 100);
+
+    //     token.approve(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD, 100);
+    //     vm.prank(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD);
+    //     token.transferFrom(address(this), 0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD, 100);
+    // }
 }
